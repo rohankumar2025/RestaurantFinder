0667dab35d7795e87aa17adc8640996aa0f989e2
rohankumar2025 <91754197+rohankumar2025@users.noreply.github.com>
2021-12-12 12:36:24 -0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,4 +1,11 @@
 #my API KEY: AIzaSyDyiSclZPTIWJyVB2B1BSMWOs9Z6e6v1dM
+
+# restaurantLabels[0] : Name of Restaurant OR Error Message if No More Available Restaurants
+# restaurantLabels[1] : $$$ indicating Price Level
+# restaurnatLabels[2] : Photo
+# restaurnatLabels[3] : Address
+# restaurantLabels[4] : Star Rating
+# restaurantLabels[5] : Number of results 
   
 # importing required modules
 import requests, json
@@ -7,102 +14,114 @@ import tkinter as tk
 from PIL import Image, ImageTk 
 import PIL 
 from io import BytesIO
+
 # global variables
 Restaurants = {}
 city = ""
 foodType = ""
 restaurantLabels = {}
 
-# url for API call
+# urls for API calls
 url = "https://maps.googleapis.com/maps/api/place/textsearch/json?"
-photo_url = "https://maps.googleapis.com/maps/api/place/photo?"
+photo_url = "https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&maxheight=400&photo_reference="
 api_key = 'AIzaSyAOkl_9l3DVkO2RtDEmp_F4mpUdDKm6AVc'
 
 
 def printLabels():
     global restaurantLabels
-    # FIXME: NEED TO CLEAR OLD LABELS BEFORE PRINTING NEW ONES
+    # Name
+    restaurantLabels[0].config(font=("Courier Bold", 30))
+    restaurantLabels[0].place(x=600, y=20)
+    # $$$ Level
+    restaurantLabels[0].config(justify='center')
+    restaurantLabels[1].place(x=750, y=475)
+    # Photo
+    restaurantLabels[2].place(x=575, y=75)
+    # Address
+    restaurantLabels[3].config(font=("Courier", 10))
+    restaurantLabels[3].place(x=525, y=425)
+    # Num Results
+    restaurantLabels[5].config(bg="#655cb5")
+    restaurantLabels[5].place(x=420, y=575)
+    
 
-    for i in range(len(restaurantLabels)):
-        restaurantLabels[i].pack() # FIX FORMATTING LATER
+def addLabel(infoIn, ErrorMessage):
+    global restaurantLabels
+
+    try:
+        restaurantLabels.append(Label(root, text=infoIn, justify="center"))
+    except:
+        restaurantLabels.append(Label(root, text=(ErrorMessage)))
+
+def getListInfo(typeIn):
+    try:
+        infoOut=Restaurants[0][typeIn]
+    except:
+        return ""
+    return infoOut
 
 
 def createLabels():
     global city, foodType, Restaurants, restaurantLabels
     restaurantLabels = [] # erases restaurantLabels list
 
-    # Key for Labels:
-    # restaurantLabels[0] : Name of Restaurant OR Error Message if No More Available Restaurants
-    # restaurnatLabels[1] : Address
-    # restaurantLabels[2] : Star Rating
-    # restaurantLabels[3] : $$$ indicating Price Level
-    # restaurantLabels[4] : Number of results left
     for widget in root.winfo_children():
-       widget.pack_forget()
-
+       widget.place_forget()
+ 
+    # adds name of Restaurant
+    addLabel(getListInfo('name'), "Sorry there are no " + foodType + " restaurants in " + city)
+    
+    # adds $$$ text
+    dollarSignsText = ""
     try:
-        restaurantLabels.append(Label(root, text=Restaurants[0]['name']))
-    except:
-        restaurantLabels.append(Label(root, text="Sorry, there are no " + foodType + " restaurants in " + city)) # Error Message 
-
-    # MAKE CODE MORE EFFICIENT USING ARRAY STORING 'name', 'formatted_address', 'rating', etc. #
-     
-    # creates $$$ text
-    try:
-        dollarSignsText = ""
-        numDollarSigns = Restaurants[0]['price_level']
-        for i in range (0, numDollarSigns):
+        for i in range(0, getListInfo('price_level')):
             dollarSignsText = dollarSignsText + "$"
-        restaurantLabels.append(Label(root, text=dollarSignsText))
     except:
-        restaurantLabels.append(Label(root, text=""))
+        dollarSignsText = ""
+    addLabel(dollarSignsText, "")
 
     # creates photo
-    photo_data = Restaurants[0]['photos']
-    full_photo_url = photo_url + "maxwidth=400&maxheight=400&photo_reference=" + photo_data[0]['photo_reference'] + "&key=" + api_key
+    full_photo_url = photo_url + getListInfo('photos')[0]['photo_reference'] + "&key=" + api_key
     response = requests.get(full_photo_url)
     img = Image.open(BytesIO(response.content))
-    img = ImageTk.PhotoImage(img)
-    image_label = tk.Label(image=img)
-    image_label.image = img
 
+    resized_img = img.resize((350,350))
+    
+    resized_img = ImageTk.PhotoImage(resized_img)
+    image_label = tk.Label(image=resized_img)
+    image_label.image = resized_img
     restaurantLabels.append(image_label)
 
-    
-
     # creates Address text
-    try: 
-        restaurantLabels.append(Label(root, text=Restaurants[0]['formatted_address']))
-    except:
-        restaurantLabels.append(Label(root, text="Address Unavailable"))
+    addLabel(getListInfo('formatted_address'), "Address Unavailable")
 
     # creates star Rating text
-    try:
-        restaurantLabels.append(Label(root, text="Rating: " + str(Restaurants[0]['rating'])))
-    except:
-        restaurantLabels.append(Label(root, text="Rating: N/A"))
-
-
+    addLabel("Rating: " + str(getListInfo('rating')), "Rating: N/A")
 
     # creates text showing number of remaining results
-    restaurantLabels.append(Label(root, text=str(len(Restaurants)) + " Results"))
-
-    # deletes current restaurant from array
-    try:
-        del Restaurants[0]
-    except:
-        pass
+    addLabel(str(len(Restaurants)) + " Results", "")
 
     rerenderTextBoxes()
     printLabels()
-    displayYNButtons()
+    displayNextButton()
 
-def displayYNButtons():
-    yesButton = Button(root, text="Yes")
-    yesButton.pack() # FIX FORMATTING LATER
+def incrementRestaurant():
+    global Restaurants
+    Restaurants.append(Restaurants.pop(0))
+    createLabels()
 
-    noButton = Button(root, text="No", command=createLabels)
-    noButton.pack() # FIX FORMATTING LATER
+def unincrementRestaurant():
+    global Restaurants
+    Restaurants = Restaurants[-1:] + Restaurants[:-1] 
+    createLabels()
+
+
+def displayNextButton():
+    prevButton = Button(root, text="Previous", command=(unincrementRestaurant))
+    prevButton.place(x=690, y=500) 
+
+    nextButton = Button(root, text="Next", command=(incrementRestaurant))
+    nextButton.place(x=765, y=500)
 
 def makeAPICall(cityLocation, restaurantType):
     # calls API, stores requests in 'req'
@@ -121,31 +140,29 @@ def handleSubmit():
     Restaurants = makeAPICall(city, foodType)
     createLabels()
 
-def rerenderTextBoxes():   
-    locationLabel.pack() # FIX FORMATTING LATER
-    locationTextBox.pack() # FIX FORMATTING LATER
-    typeFoodLabel.pack() # FIX FORMATTING LATER
-    typeFoodTextBox.pack() # FIX FORMATTING LATER
-    submitButton.pack() # FIX FORMATTING LATER
+def rerenderTextBoxes():  
+    leftFrame.place(x=0, y=0) 
+
+    locationTextBox.place(x=50, y=175) 
+    typeFoodTextBox.place(x=50, y=275) 
+    submitButton.place(x=250, y=400) 
 
 # creating GUI
 root = Tk()
 root.title('Restaurant Finder')
-root.geometry("600x600")
+root.geometry("1000x600")
+root.resizable(False, False)
 
-locationLabel = Label(root, text="Location:")
-locationLabel.pack() # FIX FORMATTING LATER
+leftFrame = Frame(root, bg="#655cb5", width=500, height=600) # creates leftside purple UI color
 
-locationTextBox = Entry(root, width=30)
-locationTextBox.pack() # FIX FORMATTING LATER
+locationTextBox = Entry(root, cursor="xterm white", width=30, font=("Bahnschrift SemiBold Condensed", 20), bg="#453d87", fg="white")
+locationTextBox.insert(0, "Location") # sets default text
 
-typeFoodLabel = Label(root, text="What type of food do you want:")
-typeFoodLabel.pack() # FIX FORMATTING LATER
+typeFoodTextBox = Entry(root, cursor="xterm white", width=30, font=("Bahnschrift SemiBold Condensed", 20), bg="#453d87", fg="white")
+typeFoodTextBox.insert(0, "What type of food do you want") # sets default text
 
-typeFoodTextBox = Entry(root, width=30)
-typeFoodTextBox.pack() # FIX FORMATTING LATER
+submitButton = Button(root, text="Submit", command=handleSubmit, bg="#453d87", fg ="white", font=("Helvetica", 10), height=1, width=10)
+rerenderTextBoxes()
 
-submitButton = Button(root, text="Submit", command=handleSubmit)
-submitButton.pack() # FIX FORMATTING LATER
 
 root.mainloop()
